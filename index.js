d3daf626e5fdd24177f006783ba1dab6f8a8add3
Aditya Gupta <adityag2010@gmail.com>
2017-07-19 19:50:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ restService.post('/finUNO', function(req, res) {
     var inputText = req.body.result.resolvedQuery;
     //var inputText = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     for(var i=0;i<scrips.length;i++){
-        if(inputText.toLowerCase().search((scrips[i].FIELD1).toLowerCase()) !== -1 || inputText.toLowerCase().search(scrips[i].FIELD2.toLowerCase()) !== -1)
+        if((inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase()) !== -1 || (inputText.toLowerCase()).search((scrips[i].FIELD2).toLowerCase()) !== -1)
            return res.json({
                 scripnames : scrips[i].FIELD1,
                 speech : scrips[i].FIELD1,
