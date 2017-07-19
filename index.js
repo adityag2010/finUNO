6fbe490876f949f14ed917c4650c94d088c99b8e
Aditya Gupta <adityag2010@gmail.com>
2017-07-19 17:52:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,17 +11,24 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
-var random = require("./EQUITY_L.json");
+var scrips = require("./EQUITY_L.json");
 //const scrip_name = random.split(",");
 
 restService.post('/finUNO', function(req, res) {
     var inputText = req.body.result.resolvedQuery;
     //var inputText = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    return res.json({
-        speech: random[0].FIELD1,
-        displayText: inputText,
+    for(var i=0;i<scrips.length;i++){
+        if(inputText.search(scrips[0].FIELD1||scrips[1].FIELD2) !== -1)
+            return res.json({
+                scrip-names : scrips[0].FIELD1,
+                speech : scrips[0].FIELD1
+            });
+     }
+    /*return res.json({
+        speech: scrips[0].FIELD1,
+        //displayText: inputText,
         //source: 'webhook-financial-assistant'
-    });
+    });*/
 });
 
 restService.listen((process.env.PORT || 8000), function() {
