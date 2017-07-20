9e944a0b276a8c91244310fa8fdeebb102d14303
Aditya Gupta <adityag2010@gmail.com>
2017-07-20 15:53:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var scrips = require("./EQUITY_L.json");
 restService.use(bodyParser.json());
 
 restService.post('/finUNO', function(req, res) {
-    //var inputText= req.body.result.resolvedQuery;
+    var inputText= req.body.result.resolvedQuery;
     //var json_copy  = JSON.parse(JSON.stringify(req));
     //json_copy.body.result.scripnames  = "Aditya";
     //res = JSON.parse(JSON.stringify(json_copy));
