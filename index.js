e75fa717c463938f875742bb2ef644e4090cff6f
Aditya Gupta <adityag2010@gmail.com>
2017-07-20 12:45:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,19 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
+var scrips = require("./EQUITY_L.json");
+
 restService.post('/finUNO', function(req, res) {
-    var inputText = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    var inputText = req.body.result.parameters.echoText;
     return res.json({
+        speech : scrips[0].FIELD1,
+        displayText : "I hope this works now atleast"
+    })
+    /*return res.json({
         speech: inputText,
         displayText: inputText,
         source: 'webhook-financial-assistant'
-    });
+    });*/
 });
 
 restService.listen((process.env.PORT || 8000), function() {
