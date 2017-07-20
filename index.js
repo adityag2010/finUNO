5607cd1e2eb17cc6322b5d4095ee32930d2e7226
Aditya Gupta <adityag2010@gmail.com>
2017-07-20 11:53:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,16 +20,19 @@ restService.post('/finUNO', function(req, res) {
         speech : random.toLowerCase().search((scrips[0].FIELD2).toLowerCase())
     });*/
     var inputText = req.body.result.resolvedQuery;
+    var json_copy = req;
+    json_copy.body.result.scripnames = "Aditya";
     //req.body.result.scripnames = :"aditya";
     //var inputText = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     for(var i=0;i<scrips.length;i++){
         if((inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase()) !== -1 || (inputText.toLowerCase()).search((scrips[i].FIELD2).toLowerCase()) !== -1)
-           return res.json({
+          res.send(json_copy);
+            /* return res.json({
                 //scripnames : scrips[i].FIELD1,
                 //speech : scrips[i].FIELD1,
                 displayText : scrips[i].FIELD1
                 
-            });
+            });*/
     }
     /*return res.json({
         speech: scrips[0].FIELD1,
