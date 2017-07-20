23ed5b7da8a36421c427fc196864baf3bf3cc5f0
Aditya Gupta <adityag2010@gmail.com>
2017-07-20 18:50:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,14 @@ restService.use(bodyParser.json());
 
 restService.post('/finUNO', function(req, res) {
     var inputText= req.body.result.resolvedQuery;
-    var json_copy = req;
-    var random = json_copy.body.result.resolvedQuery;
-    json_copy.body.result.parameters.scripnames = "Aditya";
-    //random = json_copy.body.result.parameters.scripnames;
-    //res = json_copy;
-    
+    var buy_sell = req.body.result.parameters.buy_sell;
+    var exchange = req.body.result.parameters.exchange;
+    var quantity = req.body.result.parameters.quantity;
+    var price_type = req.body.result.parameters.price_type;
+    var product_type = req.body.result.parameters.product_type;
+    var shares = req.body.result.parameters.shares;
+    var validity = req.body.result.parameters.validity;
+   
     for(var i=0;i<scrips.length;i++){
         if((inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase()) !== -1 || (inputText.toLowerCase()).search((scrips[i].FIELD2).toLowerCase()) !== -1)
             return res.json({
@@ -47,11 +49,18 @@ restService.post('/finUNO', function(req, res) {
         followupEvent : {
             name : "trade_slot",
             data : {
+                buy_sell : buy_sell,
+                exchange : exchange,
+                quantity : quantity,
+                price_type : price_type,
+                product_type : product_type,
+                shares : shares,
+                validity : validity,
                 scripnames : "Gupta"
             }
         },
         displayText: scrips[0].FIELD1,
-        source : random
+        source : inputText
     });
 });
  
