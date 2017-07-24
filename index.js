f6e987781bb96764f038c50c85ef4e61f13e8dfa
Aditya Gupta <adityag2010@gmail.com>
2017-07-24 12:16:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ restService.post('/finUNO', function(req, res) {
     var shares = req.body.result.parameters.shares;
     var validity = req.body.result.parameters.validity;
    */
-    for(var i=0 ; i < scrips ; i++){
+    for(var i=0 ; i < scrips.length ; i++){
         if((inputText.toLowerCase()).search((scrips[i].FIELD1).toLowerCase()) !== -1 || (inputText.toLowerCase()).search((scrips[i].FIELD2).toLowerCase()) !== -1)
             return res.json({
                 contextOut : [{
