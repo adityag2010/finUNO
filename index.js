1519bf633c538bdfa750bdd3b18b500f37faaf71
Aditya Gupta <adityag2010@gmail.com>
2017-07-24 14:47:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,15 +68,7 @@ restService.post('/finUNO', function(req, res) {
         followupEvent : {
             name : "trade_slot",
             data : {
-               /* buy_sell : "buy",
-                exchange : "NSE",
-                quantity : 200,
-                price_type : price_type,
-                product_type : product_type,
-                shares : shares,
-                validity : validity,
-               */ scripnames : "Gupta",
-               // random : "Whatever"
+                scripnames : "Gupta"
             }
         },
         displayText: scrips[0].FIELD1,
